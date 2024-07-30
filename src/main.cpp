140d695cd6a8fded47c82cb5c4ff00e5333edc41
bishakh-dutta <bishakh2003dutta@gmail.com>
2024-07-30 18:15:01 +0530

    added test file for lexer

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,6 @@
 #include "lexer/lexer.hpp"
 using namespace std;
 string readSourceCode(const string& path);
-string getTokenTypeName(TokenType type);
-
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         cerr << argv[0] << "Enter file path" << endl;
@@ -18,12 +16,8 @@ int main(int argc, char* argv[]) {
     catch(const exception& e){
             cerr<<"Error: "<<e.what()<<'\n';
     }
-    
     Lexer lexer(sourceCode);
     vector tokens = lexer.getTokens();
-    for(const Token i : tokens){
-        cout << "[  " + getTokenTypeName(i.type) + " : " + i.lexeme + "       (Line: " + to_string(i.line) + " Col: " + to_string(i.column) + ") ]" << endl;
-    }
 }
 
 string readSourceCode(const string& path){
@@ -37,22 +31,4 @@ string readSourceCode(const string& path){
         codeStr+=line+'\n';
     }
     return codeStr;
-}
-
-string getTokenTypeName(TokenType type) {
-    switch (type) {
-        case KEYWORD: return "KEYWORD";
-        case IDENTIFIER: return "IDENTIFIER";
-        case OPERATOR: return "OPERATOR";
-        case EQUALS: return "EQUALS";
-        case LITERAL: return "LITERAL";
-        case INT: return "INT";
-        case FLOAT: return "FLOAT";
-        case BOOL: return "BOOL";
-        case STRING: return "STRING";
-        case INDENT: return "INDENT";
-        case DEDENT: return "DEDENT";
-        case NUL: return "NUL";
-        default: return "UNKNOWN";
-    }
 }
---
+++ tests/lexer_tests/lexer_test.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<fstream>
+#include "src/lexer/lexer.hpp"
+using namespace std;
+string readSourceCode(const string& path);
+string getTokenTypeName(TokenType type);
+void printCode(vector<Token> tokens);
+int main(){
+    string filePath = "./tests/lexer_tests/lexer_test.prv";
+    string sourceCode;
+    try {
+        sourceCode = readSourceCode(filePath);
+    }
+    catch(const exception& e){
+            cerr<<"Error: "<<e.what()<<'\n';
+    }
+    Lexer lexer(sourceCode);
+    vector tokens = lexer.getTokens();
+    
+    printCode(tokens);
+    for(const Token i : tokens){
+        cout << "[  " + getTokenTypeName(i.type) + " : " + i.lexeme + "       (Line: " + to_string(i.line) + " Col: " + to_string(i.column) + ") ]" << endl;
+    }
+}
+string readSourceCode(const string& path){
+    ifstream sourceCode(path);
+    if(!sourceCode.is_open()){
+        throw runtime_error("Unable to open file: "+path+" (Please check if file exits at the specified path)");
+    }
+    string codeStr;
+    string line;
+    while(getline(sourceCode,line)){
+        codeStr+=line+'\n';
+    }
+    return codeStr;
+}
+string getTokenTypeName(TokenType type) {
+    switch (type) {
+        case KEYWORD: return "KEYWORD";
+        case IDENTIFIER: return "IDENTIFIER";
+        case OPERATOR: return "OPERATOR";
+        case EQUALS: return "EQUALS";
+        case LITERAL: return "LITERAL";
+        case INT: return "INT";
+        case FLOAT: return "FLOAT";
+        case BOOL: return "BOOL";
+        case STRING: return "STRING";
+        case INDENT: return "INDENT";
+        case DEDENT: return "DEDENT";
+        case NUL: return "NUL";
+        default: return "UNKNOWN";
+    }
+}
+
+void printCode(vector<Token> tokens){
+    int indent = 0,level=0,flag=0;
+    for(int i=0;i<tokens.size();i++){
+        Token t = tokens[i];
+        if(t.lexeme=="INDENT"){
+            indent++;
+            flag=1;
+        }else if(t.lexeme=="DEDENT"){
+            indent--;
+            flag=1;
+        }else{
+            flag=0;
+        }
+        if(flag==1){
+            continue;
+        }
+        while(level<indent){
+            cout<<"\t";
+            level++;
+        }
+        cout<<t.lexeme<<" ";
+        if(tokens[i+1].line!=tokens[i].line){
+            level=0;
+            cout<<endl;
+        }
+    }
+}
